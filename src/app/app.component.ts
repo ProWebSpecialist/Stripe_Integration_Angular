a3e4556b5ee514719554d57d163745e160a6a587
Bohdan Dankovych <bohdan.dankovych@gmail.com>
2020-05-25 01:29:23 +1000

    2020-05-25 upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'stripeIntegration';
+
+  constructor() {}
+  handler:any = null;
+  ngOnInit() {
+    
+  }
+
+  pay(amount) {
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_aeUUjYYcx4XNfKVW60pmHTtI',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        console.log(token)
+        alert('Token Created!!');
+      }
+    });
+ 
+    handler.open({
+      name: 'Demo Site',
+      description: '2 widgets',
+      amount: amount * 100
+    });
+  }
+}
